00406fb5b09d013e86408d4c80328bf52e6f756e
JoelXu <xmasuhai@163.com>
2020-03-23 02:00:08 +0800

    update article 【算法与数据结构02】排序算法（下）& upload demo data-structure-1

---
+++ demo-test/data-structure-1/src/main.js
@@ -0,0 +1,39 @@
+const divScreen = document.querySelector('#screen')
+const btnCreateNumber = document.querySelector('#createNumber')
+const btnCallNumber = document.querySelector('#callNumber')
+    /* 取到 当前号码对应的span */
+const spanNewNumber = document.querySelector('#newNumber')
+const spanQueue = document.querySelector('#queue') // 取到 当前队列
+
+/* 监听用户取号
+ ** 声明一个号 n = 0 默认无人排队
+ ** 声明队列数组 queue
+ */
+let n = 0
+let queue = []
+
+/* 点击取号时显示 当前号码 */
+btnCreateNumber.onclick = () => {
+    n += 1
+    queue.push.call(null, n) // 将取得的好记录下来 // queue.push(n)
+    spanNewNumber.innerHTML = n
+        // spanQueue.innerText = queue.toString() // spanQueue.innerText = queue.toString()  只能显示字符串，会用逗号连接 不美观 1,2,3,4,5 变为字符串格式的[]
+        /* JSON.stringify 将对象变为带有原对象格式的字符串  */
+    spanQueue.innerText = JSON.stringify(queue)
+}
+
+/* 点击叫号时 出队 */
+btnCallNumber.onclick = () => {
+    /* 排除叫完号 出现 m === undefined */
+    if (queue.length === 0) {
+        divScreen.innerText = `取号完毕`
+        return;
+    }
+    const m = queue.shift()
+    divScreen.innerText = `请 ${m} 号就餐`
+    spanQueue.innerText = JSON.stringify(queue) // 队列更新，再次打印到span里 放到span里
+}
+
+/*
+    队列：类似数组的结构，只提供入队queue.push(n)，和出队queue.shift()两个操作
+*/
